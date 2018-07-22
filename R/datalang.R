25e7330687be8f24c7e006bf402f2e308a7bbef9
Edgar Ruiz <edgararuiz@users.noreply.github.com>
2018-07-22 10:56:04 -0500

    Updates DESCRIPTION, adds translate_folder and improves yml spec

---
+++ R/datalang.R
@@ -0,0 +1,7 @@
+#' @import rlang
+#' @importFrom dplyr bind_cols
+#' @importFrom purrr imap
+#' @importFrom purrr walk
+#' @importFrom purrr map_chr
+#' @keywords internal
+utils::globalVariables(c("."))
--- R/translate.R
+++ R/translate.R
@@ -11,35 +11,36 @@ translate_column <- function(column, from, to) {
 }
 
 #' @export
-#' @importFrom magrittr %>%
-translate_data <- function(df = NULL, spec = NULL) {
-  if (is.null(df)) stop("Please provide a data.frame")
-  if (is.null(spec)) stop("Please provide the path of a spec")
+translate_data <- function(df = NULL, spec_path = NULL) {
 
-  spec <- yaml::read_yaml(spec)
-  purrr::imap(spec$variables, ~{
+  if (is.null(df)) stop("Please provide a data.frame")
+  if (is.null(spec_path)) stop("Please provide the path of a spec_path")
+
+  spec <- yaml::read_yaml(spec_path)
+  vars <- imap(spec$variables, ~{
     col <- df[, .y]
-    if (!is.null(.x$values)) {
-      for (i in 1:length(.x$values)) {
-        val <- names(.x$values[i])
-        col <- translate_column(col, val, .x$values[[i]])
+    vals <- .x$values
+    if (!is.null(vals)) {
+      for (i in 1:length(vals)) {
+        val <- names(vals[i])
+        col <- translate_column(col, val, vals[[i]])
       }
     }
     names(col) <- .x["trans"]
     col
-  }) %>%
-    dplyr::bind_cols()
-
+  })
+  bind_cols(vars)
 }
 
 #' @export
-create_rd <- function(df = NULL, spec = NULL) {
+create_rd <- function(df = NULL, spec_path = NULL) {
+
   if (is.null(df)) stop("Please provide a data.frame")
-  if (is.null(spec)) stop("Please provide the path of a spec")
+  if (is.null(spec_path)) stop("Please provide the path of a spec_path")
 
-  spec <- yaml::read_yaml(spec)
+  spec <- yaml::read_yaml(spec_path)
 
-  h <- list(
+  header <- list(
     "\\docType{data}",
     if(!is.null(spec$help$name)) paste0("\\name{", spec$help$name,"}"),
     if(!is.null(spec$help$alias)) paste0("\\alias{", spec$help$alias,"}"),
@@ -48,22 +49,64 @@ create_rd <- function(df = NULL, spec = NULL) {
     "\\describe{"
   )
 
-  i <- NULL
-  i <- purrr::map_chr(spec$variables, ~{
-    paste0("\\item{", .x["trans"], "}{", .x["desc"], "}")
-  })
-  names(i) <- NULL
+  items <- NULL
+  items <- map_chr(
+    spec$variables,
+    ~ paste0("\\item{", .x["trans"], "}{", .x["desc"], "}")
+    )
+  names(items) <- NULL
 
-  b <- list(
+  footer <- list(
     "}}",
     if(!is.null(spec$help$usage)) paste0("\\usage{", spec$help$usage,"}"),
     if(!is.null(spec$help$description)) paste0("\\description{", spec$help$description,"}"),
     "\\keyword{datasets}"
   )
-
-  h <- c(h, i, b)
-
-  #writeLines(as.character(h), paste0("man/", spec$help$name, ".rd"))
-  as.character(h)
+  rd <- c(header, items, footer)
+  as.character(rd)
 }
+
+save_translation <- function(df, spec, name, data_folder) {
+  name <- enexpr(name)
+  name_char <- as.character(name)
+  e <- global_env()
+  e[[name_char]] <- translate_data(df, spec)
+  save(list = name_char, file = paste0(data_folder, "/", name_char, ".rda"))
+  rm(list = name_char, envir = e)
+}
+
+#' @export
+translate_folder <- function(spec_folder = "inst/specs", data_folder = "data", rd_folder = "man"){
+
+  specs <- list.files(spec_folder, pattern = "yml")
+
+  walk(specs, ~{
+
+    spec_path <- file.path(spec_folder, .x)
+
+    spec <- yaml::read_yaml(spec_path)
+
+    df <- parse_expr(spec$df$source)
+    df <- eval(df)
+
+    name <- parse_expr(spec$df$name)
+
+    if(!is.null(data_folder)){
+      save_translation(
+        df =   df,
+        spec = spec_path,
+        name = !!name,
+        data_folder = data_folder
+      )
+    }
+
+    if(!is.null(rd_folder)){
+      rd <- create_rd(df, spec_path)
+      writeLines(rd, con = file.path(rd_folder, paste0(spec$df$name, ".rd")))
+    }
+
+  }
+  )
+}
+
 

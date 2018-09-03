d7459dec8aa49ca572ebadb227394a5210b7e83d
Edgar Ruiz <edgararuiz@gmail.com>
2018-09-03 14:27:19 -0500

    Adds help for rd functions

--- R/datalang.R
+++ R/datalang.R
@@ -3,5 +3,6 @@
 #' @importFrom tibble is_tibble
 #' @importFrom tibble as_tibble
 #' @importFrom yaml read_yaml
+#' @importFrom rstudioapi viewer
 #' @keywords internal
 utils::globalVariables(c("."))
